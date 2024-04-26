e9b67b17798aa552580ea6004d2f7e57dac28fed
diegarlin <garcialinaresdiego@gmail.com>
2024-04-26 13:22:44 +0200

    Add email to model and all logic in routes

--- myapp/models.py
+++ myapp/models.py
@@ -4,8 +4,9 @@ from .extensions import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)  
     password_hash = db.Column(db.String(120), nullable=False)
     admin = db.Column(db.Boolean, default=False)
     deviceID = db.Column(db.String(120), nullable=False)
     profesor = db.Column(db.Boolean, nullable=True, default=None)
-    despacho = db.Column(db.String(10),  nullable=True, default=None)
+    despacho = db.Column(db.String(10), nullable=True, default=None)
--- myapp/routes.py
+++ myapp/routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from .extensions import bcrypt
 from .models import User, db
+import re
 
 main = Blueprint('main', __name__)
 
@@ -13,14 +14,27 @@ def hola():
 @main.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
+    username = data.get('username').strip()
+    password = data.get('password').strip()
+    email = data.get('email').strip()
     deviceID = data.get('deviceID')
+
+    # Verificar si hay espacios en blanco en el username
+    if ' ' in username:
+        return jsonify({"msg": "El nombre de usuario no puede contener espacios en blanco"}), 400
+
+    # Verificar si hay espacios en blanco en el password
+    if ' ' in password:
+        return jsonify({"msg": "La contraseña no puede contener espacios en blanco"}), 400
 
     # Verificar si el usuario ya existe
     existing_user = User.query.filter_by(username=username).first()
     if existing_user:
         return jsonify({"msg": "El nombre de usuario ya está en uso"}), 400
+    
+    existing_email = User.query.filter_by(email=email).first()
+    if existing_email:
+        return jsonify({"msg": "El correo ya está en uso"}), 400
 
     # Verificar si el deviceID ya está en uso
     existing_device = User.query.filter_by(deviceID=deviceID).first()
@@ -31,10 +45,14 @@ def register():
     if not (len(password) >= 6 and any(c.isupper() for c in password) and any(c.islower() for c in password) and any(c.isdigit() for c in password)):
         return jsonify({"msg": "La contraseña debe tener al menos 6 caracteres, una mayúscula, una minúscula y un número"}), 400
 
+    # Verificar si el email es válido
+    if not validate_email(email):
+        return jsonify({"msg": "El correo electrónico no es válido"}), 400
+
     # Creamos el hash y lo almacenamos en la base de datos
     hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
     
-    new_user = User(username=username, password_hash=hashed_password, deviceID=deviceID)
+    new_user = User(username=username, password_hash=hashed_password, email=email, deviceID=deviceID)
     db.session.add(new_user)
     db.session.commit()
 
@@ -43,26 +61,30 @@ def register():
 @main.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
+
+    username_or_email = data.get('username_or_email', '').strip()
+    password = data.get('password', '').strip()
     deviceID = data.get('deviceID')
 
-    user = User.query.filter_by(username=username).first()
+    if not username_or_email or not password:
+        return jsonify({"msg": "El nombre de usuario/correo electrónico y la contraseña son obligatorios"}), 400
 
-    if user:
-        if bcrypt.check_password_hash(user.password_hash, password):
-            # Verificar si el deviceID ha cambiado
-            if user.deviceID != deviceID:
-                user.deviceID = deviceID
-                db.session.commit()
+    # Verificar si el username_or_email es un nombre de usuario válido
+    user = User.query.filter_by(username=username_or_email).first()
 
-            # Creamos el token con el cual podrán mandar llamadas a la api
-            access_token = create_access_token(identity=username)
-            return jsonify(access_token=access_token), 200
-        else:
-            return jsonify({"msg": "Contraseña inválida"}), 401
-    else:
-        return jsonify({"msg": "Usuario inválido"}), 401
+    # Si no encontramos un usuario con ese nombre de usuario, intentamos buscar por email
+    if not user:
+        user = User.query.filter_by(email=username_or_email).first()
+
+    if not user or not bcrypt.check_password_hash(user.password_hash, password):
+        return jsonify({"msg": "Nombre de usuario/correo electrónico o contraseña incorrectos"}), 401
+
+    # Verificar si el deviceID ha cambiado
+    if user.deviceID != deviceID:
+        user.deviceID = deviceID
+        db.session.commit()
+
+    return jsonify({"msg": "Inicio de sesión exitoso"}), 200
 
 # Esta función sirve para ver si funciona tu token
 @main.route('/protected', methods=['GET'])
@@ -70,3 +92,7 @@ def login():
 def protected():
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
+
+def validate_email(email):
+    email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    return re.match(email_regex, email) is not None
