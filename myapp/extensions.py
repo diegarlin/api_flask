b7ae6bd2f541989821417c07eafd97da95242a23
diegarlin <garcialinaresdiego@gmail.com>
2024-05-09 10:20:33 +0200

    chore: Add Flask-Mail extension for email functionality

--- myapp/extensions.py
+++ myapp/extensions.py
@@ -2,7 +2,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_mail import Mail
 
+mail = Mail()
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 jwt = JWTManager()
